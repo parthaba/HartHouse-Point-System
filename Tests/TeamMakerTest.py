0e9f2827123a7c4f79a6050bf5306f97c6b6fdf9
Arpi <jamesrp.yang@mail.utoronto.ca>
2021-10-29 21:22:34 -0400

    added more debater data

--- Tests/TeamMakerTest.py
+++ Tests/TeamMakerTest.py
@@ -1,13 +1,14 @@
 from Classes.TeamMaker import TeamMaker
+from Classes.Qualifier import Qualifier
 import unittest
 
-class TestTeamMaker(unittest.TestCase):
 
-    team_list = [[463871, 463871], [463871, 463871], [463871, 463871]]
+class TestTeamMaker(unittest.TestCase):
+    team_list = [[463871, 119567], [829380, 459855]]
     new_qual_contingent = TeamMaker(3)
     new_qual_contingent.team_list = team_list
 
+    new_qual = Qualifier
+
     def test_create_team_list(self):
         contingent_list = self.new_qual_contingent.create_team_list()
-
-
